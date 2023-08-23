902613ee0f7aba6c8512c775298a0dbb8e422b22
Bill Reiss <billreiss@users.noreply.github.com>
2023-08-23 14:54:44 -0400

    Added repro for SOAP issue

---
+++ 16957/Maui8App2/Maui8App2/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace Maui8App2
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private async void CounterBtn_Clicked(object sender, EventArgs e)
+		{
+			try
+			{
+				var client = new ServiceReference1.Service1Client(ServiceReference1.Service1Client.EndpointConfiguration.BasicHttpsBinding_IService1);
+				var result = await client.GetDataAsync(1);
+				helloLabel.Text = $"Successful WCF Call. Return Value = {result}";
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+				helloLabel.Text = $"Error calling WCF Service. {ex.Message}";
+			}
+		}
+	}
+
+}
---
+++ 16957/Maui8App2/Maui8App2/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Maui8App2
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
